c228cbb1b0d951457ab9b3c414f6d1aeb508e3db
Aadit <god@shastra.local>
2024-11-19 06:08:08 +0000

    first

---
+++ main.py
@@ -0,0 +1,9 @@
+from structured_attention_graphs.generation import generate_sag
+
+
+
+roots = generate_sag('structured_attention_graphs/Images/peacock/n01806143_225.JPEG')
+print(roots)
+
+
+# generation.generate_sag()
